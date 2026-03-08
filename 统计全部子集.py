0540583d34b2a80f7accc4d5476ba3fd2a3fbfad
NING <2500012296@stu.pku.edu.cn>
2026-03-08 14:31:48 +0800

    docs: 补充时间复杂度和代码注释

--- 统计全部子集.py
+++ 统计全部子集.py
@@ -17,3 +17,4 @@ def generate_subsets(arr):
     return result
 a=list(map(int, input().split()))
 print(generate_subsets(a))
+#上述代码中，fibonacci_recursive 函数每次递归都会分裂成两个子问题，递归树的节点总数为 2^n，因此时间复杂度为O(2^n)generate_subsets 函数通过回溯法枚举所有子集，每个元素有选或不选两种选择，子集总数为2^n,所以整体时间复杂度也是O(2^n)
